be1173c4ef392ae11b4cc91919f1ab19c94a7c0f
RamHolt94 <Rkholt9427@hotmail.com>
2024-03-15 23:04:23 -0400

    Lesson 7 Completed

--- lesson_7/homework.py
+++ lesson_7/homework.py
@@ -12,10 +12,10 @@
 # Complete the following code in such a way that this loop prints the characters 
 # of `name` one at a time.
 
-# name = "Joseph"
+#name = "Joseph"
 
-# for ??? in ???:
-    # print(???)
+#for name in name:
+#     print(name)
 
 # ---------------------------------------------------------------------
 # Task 2. Create a basic `for` loop with a counter
@@ -23,11 +23,16 @@
 # Complete the following code in such a way that the loop increments the counter
 # and prints the number of characters in `name`name at the end.
 
-# name = 'Tom'
-# counter = ???
+name = 'Tom'
+counter = 0
 
-# for ??? in ???:
-    # counter = ???
+for character in "Tom" :
+    counter = counter + 1
+   # print(len(name))
+    print(counter)
+if counter == 3:
+    print(len(name))
+    print('Done')
 
 # This should print '3'
 # print(counter)
@@ -39,35 +44,34 @@
 
 # 🔥 Hint: Think of it as: while counter is under 5, increment the counter and print its value🔥
 
-"""
-This should print:
-1
-2
-3
-4
-5
-"""
-# counter = ???
+# """
+# This should print:
+# 1
+# 2
+# 3
+# 4
+# 5
+# """
+counter = 0
 
-# while counter ???:
-    # counter = ???
-    # print(counter)
+while counter < 5:
+     counter = counter + 1
+     print(counter)
 
 # ---------------------------------------------------------------------
 # Task 4. Exit a loop using break 🛑
 
-# Take the previous example, and modify it so you exit the loop after five iterations, 
+# Take the previous example, and modify it so you exit the loop after five iterations,
 # but this time do it using break.
 
-# counter = ???
+counter = 0
 
-# while ???:
-    # counter = ???
+while counter < 6:
+    counter = counter + 1
+    if counter == 6:
+        break
+    print(counter)
 
-    # if ???:
-        # ???
-        
-    # print(counter)
 
 # ---------------------------------------------------------------------
 # Task 5. Range
@@ -78,13 +82,13 @@ This should print:
 # Figure out the values required for range() to generate the expected output.
 
 # 0, 1, 2, 3, 4, 5 (use only one argument)
-# range(???)
+range(6)
 
 # 0, 1, 2, 3, 4, 5 (use two arguments: start and end)
-# range(???)
+range(0, 6)
 
 # Odd numbers between 0 and 10: 1, 3, 5, 7, 9
-# range(???)
+range(1, 11, 2)
 
 # ---------------------------------------------------------------------
 # Task 6. Using range() in a loop
@@ -93,39 +97,41 @@ This should print:
 
 # Complete the following code so it prints the even numbers between 0 and 10;
 
-# for ??? in range(???):
-    # print(???)
-
+for number in range(2,10,2):
+     print(number)
 
 # Exercises 🏋🏻
 
 # ---------------------------------------------------------------------
 # Exercise 1. Digits Only!
 
-# Part one: Given a string of letters and digits, complete the program to print only the digits. 
+# Part one: Given a string of letters and digits, complete the program to print only the digits.
 # For example, for the string '3catsand5tacos', output should be: 3 5
 
 # Strategy:
 # - Create variable to hold the string: my_string = '3catsand5tacos'
 # - Create a string to represent the numbers: numbers = '1234567890'
-# - Create a loop to iterate through characters of my_string. 
+# - Create a loop to iterate through characters of my_string.
 # - If the character is a digit (`if character in numbers`) print it.
 
-# my_string = 's0m3 str1ng w1th numb3r5'
-# numbers = '1234567890'
+my_string = '3catsand5tacos'
+numbers = '1234567890'
 
-# for ??? in ???:
-    # if ??? in ???:
-    #     print(character)
+for character in my_string:
+    if character in numbers:
+         print(character)
+
 
 
 # Part two: Modify the code to print the first digit only
-
+for numbers in my_string:
+    if numbers == '3':
+      print(numbers)
 # ---------------------------------------------------------------------
 # Exercise 2. Vowel Counter
 
-# Imagine you're working on a text analysis tool that needs to count the number of vowels in a given string. 
-# As a simple practice, you have been provided with a famous quote. 
+# Imagine you're working on a text analysis tool that needs to count the number of vowels in a given string.
+# As a simple practice, you have been provided with a famous quote.
 # Your task is to count and display the total number of vowels in this quote.
 
 # quote = "Life is like riding a bicycle. To keep your balance, you MUST keep moving."
@@ -142,7 +148,7 @@ This should print:
 # ---------------------------------------------------------------------
 # Exercise 3. Sum of all Digits 🔢
 
-# You have a mixed string that contains both letters and numbers, like an alphanumeric password or 
+# You have a mixed string that contains both letters and numbers, like an alphanumeric password or
 # a serial key. Your task is to find all the numbers in this string and sum them up.
 
 # Hint: You can put the numbers you find into a list (cast as `int`) and use `sum()` on the list at the end.
@@ -151,16 +157,22 @@ This should print:
 # digits = "0123456789"
 # found_digits = ???
 
-# for char in mixed_string:
-    # if char in ???:
-        # found_digits.a???(int(char))
+mixed_string = "abc123xyz456"
+digits = "0123456789"
+found_digits = []
+
+for char in mixed_string:
+     if char in digits:
+         found_digits.append(int(char))
+
+print(sum(found_digits))
 
 # print(f"The total sum of numbers in the string is: {sum(found_digits)}")
 
 # ---------------------------------------------------------------------
 # Exercise 4. Password Strength Checker
 
-# You are helping to develop a user registration page for a website. As part of the registration process, 
+# You are helping to develop a user registration page for a website. As part of the registration process,
 # you need to ensure that submitted passwords are strong. A strong password should have at least 8 characters.
 
 # Create a Python program to check the strength of a list of passwords and count how many are strong.
@@ -168,27 +180,30 @@ This should print:
 # passwords = ['Passw0rd', 'hello', 'strongPass1', 'weak']
 # strong_password_count = 0
 
-# for ??? ??? ???:
-    # if ???(password) >= 8:
-        # strong_password_count ??? 1
+passwords = ['Passw0rd', 'hello', 'strongPass1', 'weak']
+strong_password_count = 0
 
-# print(f"Number of strong passwords: {strong_password_count}")
+#
+for password in passwords:
+       if len(password) >= 8:
+        strong_password_count += 1
+
+print(f"Number of strong passwords: {strong_password_count}")
 
 # ---------------------------------------------------------------------
 # Exercise 5. The Red Crayon 🖍️
 
-# Imagine you have a box of crayons, and you're looking for a "Red" crayon. 
-# You pull out one crayon at a time from the box. 
+# Imagine you have a box of crayons, and you're looking for a "Red" crayon.
+# You pull out one crayon at a time from the box.
 
 # Use a while loop to simulate this scenario.  As soon as you find the "Red" crayon, stop the loop.
 
-# colors = ["Blue", "Yellow", "Green", "Red", "Purple", "Orange"]
-# index = 0
+colors = ["Blue", "Yellow", "Green", "Red", "Purple", "Orange"]
+index = 0
 
-# This should basically say: while the current color being evaluated is 
+# This should basically say: while the current color being evaluated is
 # different than "Red", increment to the next color and try again.
-# while colors[index] ??? "Red":
-    # print(f"Found {colors[index]} crayon. Still looking for Red.")
-    # index ??? 1
-
-# print("Found the Red crayon!")
+while colors[index] == "Red":
+    print(f"Found {colors[index]} crayon. Still looking for Red.")
+    index += 1
+print("Found the Red crayon!")
